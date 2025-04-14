ce47cd03b39c58b809837ee67f16d64994f4e417
AbdulazizAlSayyed <abdulazizsayed04@gmail.com>
2025-04-14 14:31:01 +0300

    Your meaningful commit message here

--- frontend/js/seemorejob.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('seeMoreBtn').addEventListener('click', function() {
-    let hiddenItems = document.querySelectorAll('#portfolio-container .hidden');
-    hiddenItems.forEach(item => item.classList.remove('hidden'));
-    this.style.display = 'none'; // Hide button after revealing items
-});
